3f4f764c7cfa1f1d603165e2465a3f2d085bbbc5
George Patistas <georgepatistas13@gmail.com>
2026-05-01 19:45:25 +0300

    Added more stuff

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,30 @@
 #!/usr/bin/env node
-import fs from "fs";
 
-const hello = (message: string) => {};  
+import { Command } from "commander";
 
-console.log("wow 123456789 !!!!!!!");
+// Declare the program 
+
+const program = new Command();
+
+// Add actions onto that CLI
+program
+    .argument("<string>", "strings to log")
+    .action((message: string) => {
+        console.log(`Hello ${message}`);
+    })
+    .description('Say hello');
+
+program.command("add <numbers...>").action((numbers: number[]) => {
+    const total = numbers.reduce((a, b) => a + b, 0);
+    console.log(`The total is ${total}`);
+}).description('Add numbers and log the total');
+
+program
+    .command("get-max-number <numbers...>").action((numbers: number[]) => {
+    const max = Math.max(...numbers);
+    console.log(`The maximum number is ${max}`);
+}).description('Get the maximum number from a list of numbers');
+
+// Execute the CLI with the given arguments
+
+program.parse(process.argv);
